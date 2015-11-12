0f1b07b371db3ec5d971ce1f42991e6f1634498a
François Fournier <francois.fournier@simitless.com>
2015-11-12 16:25:24 +0100

    update release

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'fafournier:eu-vat',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Tools to work with EU-VAT.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/fafournier/eu-vat/',
+  git: 'https://github.com/fafournier/meteor-eu-vat/',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
